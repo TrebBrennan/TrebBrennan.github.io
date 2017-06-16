91837846f8f2fb9cd87ebcdfdfef3f6860e21e83
Trevor Brennan <treb.brennan@gmail.com>
2017-06-16 16:11:52 +1000

    linking pages together

---
+++ test-layout/components/footer.js
@@ -0,0 +1,22 @@
+
+var html = `
+<footer>
+  <div class="wrapper clearfix">
+    <div class="coat-of-arms">
+      <img src="img/gov-logo.png"/>
+    </div>
+    <div class="links">
+      <ul class="nav">
+        <li><a href="#">Privacy policy</a></li>
+        <li><a href="#">Terms and conditions</a></li>
+        <li><a href="#">Need help?</a></li>
+        <li><a href="#">Accessibility</a></li>
+        <li><a href="#">Security</a></li>
+      </ul>
+      <p>&copy; Commonwealth of Australia. With the exception of the Commonwealth Coat of Arms and where otherwise noted, this work is licensed under the CC BY 4.0 license.</p>
+    </div>
+  </div><!--wrapper-->
+</footer>
+`;
+
+document.write(html);
