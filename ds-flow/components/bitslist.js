3a812e46364d0a988c6d853e314eaac47b562f3e
Trevor Brennan <treb.brennan@gmail.com>
2017-08-24 17:13:57 +1000

    setup prototype on git-pages

---
+++ ds-flow/components/bitslist.js
@@ -0,0 +1,64 @@
+
+var html = `
+<div class="bits-list sidebar">
+
+  <input type="search" placeholder="search patterns">
+  <br><br>
+
+  <h4>Core</h4>
+  <ul>
+    <li><a href="component.html">Principles</a></li>
+    <li><a href="component.html">Variables</a></li>
+  </ul>
+
+  <h4>Foundations</h4>
+  <ul>
+    <li><a href="component.html">Colours</a></li>
+    <li><a href="component.html">Typeface</a></li>
+    <li><a href="component.html">Grids</a></li>
+    <li><a href="component.html">Spacing</a></li>
+    <li><a href="component.html">Responsive breakpoints</a></li>
+    <li><a href="component.html">Iconography</a></li>
+    <li><a href="component.html">Headings &amp; body copy</a></li>
+  </ul>
+
+  <h4>Components</h4>
+  <ul>
+    <li><a href="component.html">Tags</a></li>
+    <li><a href="component.html">Badges</a></li>
+    <li><a href="component.html">Switch(toggle) buttons</a></li>
+    <li><a href="component.html">Basic tables</a></li>
+    <li><a href="component.html">Complex tables</a></li>
+    <li><a href="component.html">Loading</a></li>
+    <li><a href="component.html">Tooltips</a></li>
+    <li><a href="component.html">codetabs</a></li>
+    <li><a href="component.html">Accordions</a></li>
+    <li><a href="component.html">Progress indicators</a></li>
+    <li><a href="component.html">Notifications</a></li>
+    <li><a href="component.html">Buttons</a></li>
+    <li><a href="component.html">Lists</a></li>
+    <li><a href="component.html">Quotations</a></li>
+    <li><a href="component.html">Charts &amp; graphs</a></li>
+    <li><a href="component.html">Labels</a></li>
+    <li><a href="component.html"></a></li>
+  </ul>
+
+  <h4>Patterns</h4>
+  <ul>
+    <li><a href="component.html">Errors</a></li>
+    <li><a href="component.html">Breadcrumbs</a></li>
+    <li><a href="component.html">Media</a></li>
+    <li><a href="component.html">Search</a></li>
+    <li><a href="component.html">Social share</a></li>
+    <li><a href="component.html">Navigation</a></li>
+  </ul>
+
+  <h4>! Excluded</h4>
+  <ul>
+    <li><a href="component.html">Carousel</a></li>
+  </ul>
+
+</div>
+`;
+
+document.write(html);
---
+++ ds-flow/components/ribbon.js
@@ -0,0 +1,12 @@
+
+var html = `
+<div class="ribbon">
+  <div class="wrapper clearfix">
+    <div class="govau"><img src="img/govau-star.svg" />GOV.AU</div>
+    <div class="official">An official website of the Australian Government</div>
+    <button><span>How to check</span></button>
+  </div>
+</div><!--ribbon-->
+`;
+
+document.write(html);
