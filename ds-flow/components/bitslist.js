44466662facbd216092079527e2515e3497ea919
Trevor Brennan <treb.brennan@gmail.com>
2017-08-28 15:16:44 +1000

    add new layout for component pages

--- ds-flow/components/bitslist.js
+++ ds-flow/components/bitslist.js
@@ -7,55 +7,55 @@ var html = `
 
   <h4>Core</h4>
   <ul>
-    <li><a href="component.html">Principles</a></li>
-    <li><a href="component.html">Variables</a></li>
+    <li><a href="component-v2-overview.html">Principles</a></li>
+    <li><a href="component-v2-overview.html">Variables</a></li>
   </ul>
 
   <h4>Foundations</h4>
   <ul>
-    <li><a href="component.html">Colours</a></li>
-    <li><a href="component.html">Typeface</a></li>
-    <li><a href="component.html">Grids</a></li>
-    <li><a href="component.html">Spacing</a></li>
-    <li><a href="component.html">Responsive breakpoints</a></li>
-    <li><a href="component.html">Iconography</a></li>
-    <li><a href="component.html">Headings &amp; body copy</a></li>
+    <li><a href="component-v2-overview.html">Colours</a></li>
+    <li><a href="component-v2-overview.html">Typeface</a></li>
+    <li><a href="component-v2-overview.html">Grids</a></li>
+    <li><a href="component-v2-overview.html">Spacing</a></li>
+    <li><a href="component-v2-overview.html">Responsive breakpoints</a></li>
+    <li><a href="component-v2-overview.html">Iconography</a></li>
+    <li><a href="component-v2-overview.html">Headings &amp; body copy</a></li>
   </ul>
 
   <h4>Components</h4>
   <ul>
-    <li><a href="component.html">Tags</a></li>
-    <li><a href="component.html">Badges</a></li>
-    <li><a href="component.html">Switch(toggle) buttons</a></li>
-    <li><a href="component.html">Basic tables</a></li>
-    <li><a href="component.html">Complex tables</a></li>
-    <li><a href="component.html">Loading</a></li>
-    <li><a href="component.html">Tooltips</a></li>
-    <li><a href="component.html">codetabs</a></li>
-    <li><a href="component.html">Accordions</a></li>
-    <li><a href="component.html">Progress indicators</a></li>
-    <li><a href="component.html">Notifications</a></li>
-    <li><a href="component.html">Buttons</a></li>
-    <li><a href="component.html">Lists</a></li>
-    <li><a href="component.html">Quotations</a></li>
-    <li><a href="component.html">Charts &amp; graphs</a></li>
-    <li><a href="component.html">Labels</a></li>
-    <li><a href="component.html"></a></li>
+    <li><a href="component-v2-overview.html">Tags</a></li>
+    <li><a href="component-v2-overview.html">Badges</a></li>
+    <li><a href="component-v2-overview.html">Switch(toggle) buttons</a></li>
+    <li><a href="component-v2-overview.html">Basic tables</a></li>
+    <li><a href="component-v2-overview.html">Complex tables</a></li>
+    <li><a href="component-v2-overview.html">Loading</a></li>
+    <li><a href="component-v2-overview.html">Tooltips</a></li>
+    <li><a href="component-v2-overview.html">codetabs</a></li>
+    <li><a href="component-v2-overview.html">Accordions</a></li>
+    <li><a href="component-v2-overview.html">Progress indicators</a></li>
+    <li><a href="component-v2-overview.html">Notifications</a></li>
+    <li><a href="component-v2-overview.html">Buttons</a></li>
+    <li><a href="component-v2-overview.html">Lists</a></li>
+    <li><a href="component-v2-overview.html">Quotations</a></li>
+    <li><a href="component-v2-overview.html">Charts &amp; graphs</a></li>
+    <li><a href="component-v2-overview.html">Labels</a></li>
+    <li><a href="component-v2-overview.html"></a></li>
   </ul>
 
   <h4>Patterns</h4>
   <ul>
-    <li><a href="component.html">Errors</a></li>
-    <li><a href="component.html">Breadcrumbs</a></li>
-    <li><a href="component.html">Media</a></li>
-    <li><a href="component.html">Search</a></li>
-    <li><a href="component.html">Social share</a></li>
-    <li><a href="component.html">Navigation</a></li>
+    <li><a href="component-v2-overview.html">Errors</a></li>
+    <li><a href="component-v2-overview.html">Breadcrumbs</a></li>
+    <li><a href="component-v2-overview.html">Media</a></li>
+    <li><a href="component-v2-overview.html">Search</a></li>
+    <li><a href="component-v2-overview.html">Social share</a></li>
+    <li><a href="component-v2-overview.html">Navigation</a></li>
   </ul>
 
   <h4>! Excluded</h4>
   <ul>
-    <li><a href="component.html">Carousel</a></li>
+    <li><a href="component-v2-overview.html">Carousel</a></li>
   </ul>
 
 </div>
