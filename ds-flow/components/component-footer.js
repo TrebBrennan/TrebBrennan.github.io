69f1fd0abac8bff2bffa6ae6d9a345288c5fbf83
Trevor Brennan <treb.brennan@gmail.com>
2017-09-07 15:31:14 +1000

    Add feedback

---
+++ ds-flow/components/component-footer.js
@@ -0,0 +1,29 @@
+
+var html = `
+<div class="clearfix componentFooter">
+  <div class="left">
+    <h2>Feedback?</h2>
+    <ul class="smllist">
+      <li><a href="http://designsystem.trydiscourse.com/c/design-system">Community discussion</a></li>
+      <li><a href="https://github.com/govau/uikit/issues/new"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png" style="width:20px; vertical-align:middle; margin-right:0.5em"> Report an issue</a></li>
+      <li><a href="https://github.com/AusDTO/gov-au-ui-kit/blob/develop/CONTRIBUTING.md"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png" style="width:20px; vertical-align:middle; margin-right:0.5em"> Code Contribution guide</a></li>
+    </ul>
+  </div>
+  <div class="left">
+    <h2 id="">Related Articles</h2>
+    <ul class="smllist">
+      <li><a href="#">Buttons and Accessibility</a></li>
+      <li><a href="#">Visual loudness</a></li>
+    </ul>
+  </div>
+  <div class="right">
+    <h2 id="">Related Components</h2>
+    <ul class="smllist">
+      <li><a href="#">Forms</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </div>
+</div><!--componentFooter-->
+`;
+
+document.write(html);
