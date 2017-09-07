2a2f29ebe8fc2bb771c4ec2eb0450d9ac2d3e467
Trevor Brennan <treb.brennan@gmail.com>
2017-09-07 16:01:38 +1000

    Change 'the administrators' to 'the team'

--- ds-flow/components/component-footer.js
+++ ds-flow/components/component-footer.js
@@ -7,7 +7,7 @@ var html = `
       <li><a href="http://designsystem.trydiscourse.com/c/design-system">Community discussion</a></li>
       <li><a href="https://github.com/govau/uikit/issues/new"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png" style="width:20px; vertical-align:middle; margin-right:0.5em"> Report an issue</a></li>
       <li><a href="https://github.com/AusDTO/gov-au-ui-kit/blob/develop/CONTRIBUTING.md"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png" style="width:20px; vertical-align:middle; margin-right:0.5em"> Code Contribution guide</a></li>
-      <li><a href="test.html">Email the administrators</a></li>
+      <li><a href="test.html">Email the team</a></li>
     </ul>
   </div>
   <div class="left">
