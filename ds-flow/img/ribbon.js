15e69a2d46cd5df5d18654d30397d2b45ce0f68c
Matt Brooks <matthew.brooks@digital.gov.au>
2017-09-18 14:33:17 +1000

    Updating content

--- ds-flow/img/ribbon.js
+++ ds-flow/img/ribbon.js
@@ -3,8 +3,7 @@ var html = `
 <div class="ribbon">
   <div class="wrapper clearfix">
     <div class="govau"><img src="img/govau-star.svg" />GOV.AU</div>
-    <div class="official">An official website of the Australian Government</div>
-    <button><span>How to check</span></button>
+    <div class="official">An official website</div>
   </div>
 </div><!--ribbon-->
 `;
