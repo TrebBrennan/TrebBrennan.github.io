9b0d3fe49bad0408dced204d97babb736b46ffc3
Trevor Brennan <treb.brennan@gmail.com>
2017-09-21 17:13:50 +1000

    Add community page back

--- ds-flow/components/main-nav.js
+++ ds-flow/components/main-nav.js
@@ -4,9 +4,10 @@ var html = `
     <nav class="clearfix main-nav wrapper">
       <ul class="hasActive">
         <li><a href="about.html">About</a></li>
-        <li><a href="get-started.html">Getting started</a></li>
+        <li><a href="test.html">Getting started</a></li>
         <li><a href="patterns.html">Components</a></li>
         <li><a href="templates.html">Templates</a></li>
+        <li><a href="community.html">Community</a></li>
         <li><a href="support.html">Support</a></li>
         <li class="right"><a href="download.html"><img src="img/ic_file_download_white_24dp_2x.png" style="width:24px; vertical-align: middle; margin-right:0.5em;"/>Download</a></li>
         <li class="right"><a href="https://github.com/govau/uikit"><img src="img/github.png" style="width:24px; vertical-align: middle; margin-right:0.5em;"/>Github</a></li>
