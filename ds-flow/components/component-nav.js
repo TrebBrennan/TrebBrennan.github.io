3afbe7ca417943041fc2a5aace0c146bb1029ab0
MattFredBrooks <matthew.brooks@digital.gov.au>
2017-09-22 10:48:20 +1000

    Best update eva
    Bestest updates evas

---
+++ ds-flow/components/component-nav.js
@@ -0,0 +1,15 @@
+
+var html = `
+<div class="inpageNav clearfix" style="border-bottom:none">
+  <nav>
+    <ul class="hasActive">
+      <li><a href="component-v4-overview.html">Summary</a></li>
+      <li><a href="component-v4-usage.html">Usability</a></li>
+      <li><a href="component-v4-rationale.html">Accessibility</a></li>
+      <li><a href="component-v4-discussion.html">Discussion <sup>3</sup></a></li>
+    </ul>
+  </nav>
+</div>
+`;
+
+document.write(html);
---
+++ ds-flow/js/main.js
@@ -0,0 +1,57 @@
+// ============= Smooth scroll
+document.documentElement.className = 'js';
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+// ============= Active Nav Check
+// Only testewd in chrome
+
+// Get the page URL
+var url = window.location.href;
+var urlres = url.split("/");
+var urlPage = urlres[urlres.length-1];
+
+// Get all the links inside an element that has the class "hasActive"
+$('.hasActive').find('a').each(function() {
+  // Check if the URL matches the link
+  if ( $(this).attr('href') == urlPage ){
+    // Make it nice
+    $(this).addClass('active');
+  }
+});
