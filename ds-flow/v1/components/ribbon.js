172dc659857661c1d8b74f1bbd184110f60cb553
Trevor Brennan <treb.brennan@gmail.com>
2017-10-16 12:18:15 +1100

    Add new Proposed First Release mockup

---
+++ ds-flow/v1/components/ribbon.js
@@ -0,0 +1,8 @@
+
+var html = `
+<div class="alert"><div class="wrapper">
+  <strong>Prototype</strong> Demonstration purposes only - content, images, and design are not final.
+</div></div>
+`;
+
+document.write(html);
---
+++ ds-flow/v1/components/status-box.js
@@ -0,0 +1,16 @@
+
+var html = `
+<div class="statusbox">
+  <div class="clearfix">
+    <strong style="float:left; width:50%; display:block">
+<i class="fa fa-check-circle-o" aria-hidden="true"></i> Released</strong>
+  </div>
+  <hr>
+  Changelog: <a href="https://github.com/govau/uikit/blob/master/packages/buttons/CHANGELOG.md">2017 Aug 24</a><br>
+  NPM Install: <a href="https://www.npmjs.com/package/@gov.au/buttons">@gov.au/buttons</a><br>
+</div>
+`;
+
+document.write(html);
+
+//<a href="http://github.com/govau/uikit/blob/master/packages/buttons/README.md"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png" style="width:16px; vertical-align:middle; margin-right:0"> GitHub</a><br>
